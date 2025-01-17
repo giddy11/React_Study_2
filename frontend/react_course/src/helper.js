583bffdf69cbe808936cd867f0169380a116e24e
Emmanuel Pinnick <emmanuelpinnick@gmail.com>
2025-01-17 00:10:02 +0000

    It didn't work.

--- frontend/react_course/src/helper.js
+++ frontend/react_course/src/helper.js
@@ -13,11 +13,13 @@ export function signup(fullname, email, password) {
         /**
          * TODO: handle cases of null in fullname, email, password
          */
+
         users.push({
             fullname: fullname,
             email: email,
             password:password
         });
+        console.log(user)
         console.log(users)
         alert("Sign up Successfully")
     } else {
@@ -26,18 +28,15 @@ export function signup(fullname, email, password) {
 
 }
 
-export function login() {
-    let email2 = document.getElementById("email2").value;
-    let password2 = document.getElementById("password2").value;
-    let result2 = document.getElementById("result2");
+export function login(email2, password2) {
 
-    let user = users.find(item => item.email == email2 && item.password==password2);
+    let user = users.find(item => item.email === email2 && item.password === password2);
     console.log(user)
     console.log(users)
     if(user == null){
-        result2.innerText = "Invalid User Authentication"
+        alert("Invalid User Authentication")
     } else {
-        result2.innerText = `Welcome to myApp ${user.fullname}`
+        alert(`Welcome to myApp ${user.fullname}`)
     }
 
 }
--- frontend/react_course/src/pages/login.js
+++ frontend/react_course/src/pages/login.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { login } from '../helper';
 
 export const Login = () => {
+  const [email2, setEmail2] = useState("");
+  const [password2, setPassword2] = useState("");
+
   return (
     <div style={{backgroundColor: "blue", color: "white", padding: "10px"}}>
         <h1>Login Form</h1>
-        <label for="email2">Email</label>
+        <label htmlFor="email2">Email</label>
         <br/>
-        <input type="email" id="email2" placeholder="Email"/>
+        <input type="email" id="email2" placeholder="Email" value = {email2} onChange={(e) => setEmail2(e.target.value)}/>
         <br/>
-        <label for="password2">Password</label>
+        <label htmlFor="password2">Password</label>
         <br/>
-        <input type="password" id="password2" placeholder="password"/>
+        <input type="password" id="password2" placeholder="password" vlaue = {password2} onChange={(e) => setPassword2(e.target.value)}/>
         <br/>
 
-        <button onclick="login()">Login</button>
+        <button onClick={login}>Login</button>
         <br/>
         <p id="result2">-------Result-------</p>
     </div>
   )
 }
+
+export default Login
